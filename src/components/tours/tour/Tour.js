a5b009bbea0a827eeea25fb7cdcbc20bd55a419e
aseelbdoor <aseel.bdoor2001@gmail.com>
2023-04-06 15:08:38 +0000

    end of work

--- src/components/tours/tour/Tour.js
+++ src/components/tours/tour/Tour.js
@@ -1,2 +1,15 @@
-import './Tours.css';
+import './Tour.css';
+import { Link } from "react-router-dom";
 
+function Tour(prop) {
+    return (
+        <div className="top">
+            <div className="title"><h1>{prop.name}</h1></div>
+            <div className="card">
+                <div className="photo"><Link to={`/city/${prop.id}`}><img src={prop.image} alt={prop.name} /></Link></div>
+            </div>
+        </div>
+    )
+}
+
+export default Tour;
--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import './rest.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
+import { StrictMode } from 'react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
